21826273b12fb5f3fa6e3652adf763a2165f2af1
Jason.lee <gmlee.boss@gmail.com>
2019-12-30 18:07:55 +0800

    init.

---
+++ src/main/java/com/gm/family/FamilyApplication.java
@@ -0,0 +1,17 @@
+package com.gm.family;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * FamilyApplication.
+ *
+ * @author : Jason.lee
+ * @version : 1.0
+ */
+@SpringBootApplication
+public class FamilyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyApplication.class);
+    }
+}

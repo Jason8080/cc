21183da351a582b0a5d3d5c115836a8c4cc0850a
Jason.lee <gmlee.boss@gmail.com>
2019-12-30 18:30:34 +0800

    init.

--- src/main/java/com/gm/family/FamilyApplication.java
+++ src/main/java/com/gm/family/FamilyApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * FamilyApplication.
+ * 启动项目.
+ *  - 在启动项目的同时, 会将CcFramework启动
+ *  - 详见{@link com.gm.cc.boot.CcApplication}
  *
  * @author : Jason.lee
  * @version : 1.0

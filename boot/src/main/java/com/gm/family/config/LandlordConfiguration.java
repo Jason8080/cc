80994c64eab904aa185789dd889ef95ad163df3c
Jason.lee <gmlee.boss@gmail.com>
2020-01-07 15:20:45 +0800

    家庭版大项目 .

---
+++ boot/src/main/java/com/gm/family/config/LandlordConfiguration.java
@@ -0,0 +1,13 @@
+package com.gm.family.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 斗地主配置.
+ *
+ * @author : Jason.lee
+ * @version : 1.0
+ */
+@Configuration
+public class LandlordConfiguration {
+}
---
+++ boot/src/main/java/com/gm/family/config/LiveConfiguration.java
@@ -0,0 +1,13 @@
+package com.gm.family.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 直播配置.
+ *
+ * @author : Jason.lee
+ * @version : 1.0
+ */
+@Configuration
+public class LiveConfiguration {
+}
